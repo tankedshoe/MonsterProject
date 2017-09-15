9b8b1d47928734c46ed4dca2305578b876e37ee7
tankedshoe <tankedshoe@gmail.com>
2017-09-15 09:32:46 -0600

    Addded a scanner.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,5 +1,6 @@
 package monster.controller;
 
+import java.util.Scanner;
 import monster.model.MarshmallowMonster;
 
 public class MonsterController
@@ -10,5 +11,21 @@ public class MonsterController
 		System.out.println(sample);
 		MarshmallowMonster realMonster = new MarshmallowMonster("Scary", 10, 3, 4.3, true);
 		System.out.println(realMonster);
+		System.out.println("Cody is hungry, so he is going to eat a tentacle");
+		realMonster.setTentacleAmount(4.2);
+		System.out.println(realMonster);
+		
+		interactWithTheMonster(realMonster);
+	}
+	
+	private void interactWithTheMonster(MarshmallowMonster currentMonster)
+	{
+		System.out.println(currentMonster.getName() + " wants to know what to eat next");
+		System.out.println(currentMonster.getName() + " suggests arms, they have " + currentMonster.getArmCount());
+		System.out.println("How many do you want to eat?");
+		Scanner myScanner = new Scanner(System.in);
+		int consumed = myScanner.nextInt();
+		currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
+		System.out.println("Ok, now " + currentMonster.getName() + " has " + currentMonster.getArmCount() + " arms left.");
 	}
 }
--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -24,6 +24,56 @@ public class MarshmallowMonster
 		this.hasBloop = hasBloop;	
 	}
 	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getEyeCount()
+	{
+		return eyeCount;
+	}
+	
+	public int getArmCount()
+	{
+		return armCount;
+	}
+	
+	public double getTentacleAmount()
+	{
+		return tentacleAmount;
+	}
+	
+	public boolean hasBloop()
+	{
+		return hasBloop;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setEyeCount(int eyeCount)
+	{
+		this.eyeCount = eyeCount;
+	}
+	
+	public void setArmCount(int armCount)
+	{
+		this.armCount = armCount;
+	}
+	
+	public void setTentacleAmount(double tentacleAmount)
+	{
+		this.tentacleAmount = tentacleAmount;
+	}
+	
+	public void setBloop(boolean hasBloop)
+	{
+		this.hasBloop = hasBloop;
+	}
+	
 	public String toString()
 	{
 		String description = "Hi, I am a scary monster! My name is " + name + ", and I have " + eyeCount;
