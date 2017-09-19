e2dcc3732c2f8c5fdef2d086c2b0d575a5308d3e
tankedshoe <tankedshoe@gmail.com>
2017-09-19 09:41:36 -0600

    Added a display constructor into the Controller class.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -2,9 +2,17 @@ package monster.controller;
 
 import java.util.Scanner;
 import monster.model.MarshmallowMonster;
+import monster.view.MonsterDisplay;
 
 public class MonsterController
 {
+	private MonsterDisplay popup;
+	
+	public MonsterController()
+	{
+		popup = new MonsterDisplay();
+	}
+	
 	public void start()
 	{
 		MarshmallowMonster sample = new MarshmallowMonster();
--- src/monster/view/MonsterDisplay.java
+++ src/monster/view/MonsterDisplay.java
@@ -1,6 +1,18 @@
 package monster.view;
 
+import javax.swing.JOptionPane;
+
 public class MonsterDisplay
 {
-
+	public void displayText(String textToDisplay)
+	{
+		JOptionPane.showMessageDialog(null, textToDisplay);
+	}
+	
+	public String getResponse(String textToDisplay)
+	{
+		String response = "";
+		response += JOptionPane.showInputDialog(null, textToDisplay);
+		return response;
+	}
 }
